790e665e48f2d84c39263f834b89de86ac44f0ab
Antricks <33262801+Antricks@users.noreply.github.com>
2021-12-08 22:01:15 +0100

    [#1] improve insert sort

--- sorting_algorithms/insert/InsertSort.java
+++ sorting_algorithms/insert/InsertSort.java
@@ -12,20 +12,23 @@ public class InsertSort implements Sort {
         ArrayList<Integer> list = new ArrayList<>();
 
         // Insert integers
-        for (int i = 0; i < inputArr.length; i ++) {
-            for (int k= 0; k < list.size(); k++) {
-                if(list.get(k) < inputArr[i]) {
-                    list.add(k+1, inputArr[i]);
-                    continue;
+        for (int input: inputArr) {
+            int k;
+            for (k= 0; k < list.size(); k++) {
+                if (list.get(k) > input) {
+                    list.add(k, input);
+                    break;
                 }
             }
-            list.add(inputArr[i]);
+            if (k == list.size()) {
+                list.add(input);
+            }
         }
 
         // Convert List<Integer> to int[]
         int[] arr = new int[list.size()];
 
-        for(int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) {
             arr[i] = list.get(i);
         }
 
