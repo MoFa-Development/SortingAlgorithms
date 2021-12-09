499b46413719926a60944040ce9f0231d16272d7
Fabian Keßler <malikfk100@gmail.com>
2021-12-09 12:38:34 +0100

    Add test array input

--- sorting_algorithms/insert/InsertSortTest.java
+++ sorting_algorithms/insert/InsertSortTest.java
@@ -1,11 +1,24 @@
 package sorting_algorithms.insert;
 
+import java.util.stream.Stream;
+
 import sorting_algorithms.test.SortTest;
 
 public class InsertSortTest {
     public static void main(String[] args) {
         System.out.println("\n### INSERT SORT TEST ###");
-        SortTest sortTest = new SortTest(InsertSort.class);
+
+        SortTest sortTest;
+        
+        int [] intArr = Stream.of(args)
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+
+        if(intArr.length > 0)
+            sortTest = new SortTest(InsertSort.class, intArr);
+        else
+            sortTest = new SortTest(InsertSort.class);
+        
         sortTest.defaultTest();
     }
 }
--- sorting_algorithms/selection/SelectionSortTest.java
+++ sorting_algorithms/selection/SelectionSortTest.java
@@ -1,11 +1,24 @@
 package sorting_algorithms.selection;
 
+import java.util.stream.Stream;
+
 import sorting_algorithms.test.SortTest;
 
 public class SelectionSortTest {
     public static void main(String[] args) {
         System.out.println("\n### SELECTION SORT TEST ###");
-        SortTest sortTest = new SortTest(SelectionSort.class);
+        
+        SortTest sortTest;
+        
+        int [] intArr = Stream.of(args)
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+
+        if(intArr.length > 0)
+            sortTest = new SortTest(SelectionSort.class, intArr);
+        else
+            sortTest = new SortTest(SelectionSort.class);
+
         sortTest.defaultTest();
     }
 }
